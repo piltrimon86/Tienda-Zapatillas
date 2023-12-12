dda88db2fe6814aca576a55ed0b739246de7decb
Fernando Gómez <fernandogomezrecio@gmail.com>
2023-12-12 10:42:21 +0100

    Simulando la base de datos

---
+++ src/data/productos.js
@@ -0,0 +1,20 @@
+export default {
+	productos: [
+		{
+			id: '1',
+			nombre: 'Converse Standard',
+			descripcion: 'Lorem ipsum dolor sit amet',
+			precio: 89.95,
+			colores: ['negro', 'rojo', 'amarillo'],
+			tamaños: ['39', '40', '41', '42', '43', '44'],
+		},
+		{
+			id: '2',
+			nombre: 'Converse 2000',
+			descripcion: 'Consectetur adipisicing elit',
+			precio: 95.95,
+			colores: ['negro', 'rojo', 'amarillo'],
+			tamaños: ['39', '40', '41', '42', '43', '44'],
+		},
+	],
+}
